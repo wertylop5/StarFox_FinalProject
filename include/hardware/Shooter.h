4a1898744ebc6bfbadd5d2a0cd7985435509b0fa
Roddy <rt363@cornell.edu>
2020-05-18 23:22:13 -0400

    Add hardware for shooter

---
+++ include/hardware/Shooter.h
@@ -0,0 +1,55 @@
+#ifndef SHOOTER_H
+#define SHOOTER_H
+
+#include "mbed.h"
+
+// This delay sets minimum time between clicks in millis.
+#define DEBOUNCE_DELAY 15
+
+class Shooter {
+    public:
+        /**
+         * Create a debounced button for shooting.
+         * Uses singleton design pattern.
+         * @param shoot Handles shooting when button pressed.
+         */
+        static void create_shooter(PinName button_pin, void (*shoot)());
+    private:
+        /**
+         * Hide default constructor so that it cannot be instantiated
+         * using it.
+         */
+        Shooter();
+        /**
+         * Keeps track of number of shooters created.
+         * Note: can only be zero or one.
+         */
+        static int count;
+        /**
+         * Shooting handler.
+         */
+        static void (*shoot)();
+        /**
+         * Keeps track of current time. (in millis)
+         */
+        static int time;
+        /**
+         * Keeps track of last time button was pressed. (in millis)
+         */
+        static int prev_time;
+        /**
+         * mBed object to increment time.
+         */
+        static Ticker ticker;
+        /**
+         * Ticker handler.
+         */
+        static void ticker_handler();
+        static InterruptIn *button;
+        /**
+         *  Button handler.
+         */
+        static void button_handler();
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include "mbed.h"
 #include "include/Game.h"
+#include "include/hardware/Shooter.h"
 
+DigitalOut led(LED1);
+void toggle(){
+	led = !led;
+}
 
 int main() {
+	Shooter::create_shooter(PTC3, toggle);
+	
 	DigitalOut out(LED_GREEN);
 	
 	Serial pc(USBTX, USBRX);

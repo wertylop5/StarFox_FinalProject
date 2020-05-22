cba9ff01a2618a97f06e137eadf6d3ff1c1d88c3
Roddy <rt363@cornell.edu>
2020-05-21 23:22:35 -0400

    Add joystick

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "mbed.h"
 #include "include/Game.h"
+#include "Joystick.h"
 #include "include/hardware/LEDMatrix.h"
+#include "include/hardware/Shooter.h"
 
 /**
 TODO: change obstacle spawn mechanics to choose a random column
@@ -11,10 +13,10 @@ define this to enable debug print statements and disable LED matrix printing
 
 comment out to disable debug print statements and enable LED matrix printing
 */
-#define GAME_DEBUG
+// #define GAME_DEBUG 
 
 //change this to control the game's speed
-const int FPS = 60;
+const int FPS = 30;
 
 const int SECOND = 1000;
 const int REFRESH_TIME = static_cast<int>((1/FPS)*SECOND);
@@ -23,8 +25,16 @@ Player p(Game::NUM_ROWS-1, Game::NUM_COLS/2, 1, 3);
 Game g(p);
 LowPowerTicker tester;
 
-void interruptTest() {
+void on_shoot() {
 	g.handleShoot();
+}
+
+void on_joystick_left(){
+	p.moveLeft();
+}
+
+void on_joystick_right(){
+	p.moveRight();
 }
 
 int main() {
@@ -32,12 +42,25 @@ int main() {
 	
 	#ifndef GAME_DEBUG
 	LEDMatrix::create_LEDMatrix(PTD2, PTD0, PTD1);
+	Shooter::create_shooter(PTC3, on_shoot);
+	Joystick joystick(PTB3,PTB2,PTB10); // note PTB10 not used
+    joystick.init();
 	#endif
 	
 	g.init(PTB10);
 	//tester.attach(&interruptTest, 1.0f);
 	
 	while(1) {
+		Direction d = joystick.get_direction();
+		// if d is NE, E, or SE (2, 3, or 4)
+		if(d >= 2 && d <= 4){
+			on_joystick_right();
+		}
+		// if d is SW, W, or NW (6, 7, or 8)
+		else if(d >= 6 && d <= 8){
+			on_joystick_left();
+		}
+
 		if (!g.loop()) {
 			tester.detach();
 			
